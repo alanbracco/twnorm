814bd8ecc4a158199533d1344c58f97f3f36bcbe
Alan Bracco <alan.bracco@gmail.com>
2018-05-14 22:52:56 -0300

    Removed stemmer. Lemmatizer as a separated class

--- wta_classifier.py
+++ wta_classifier.py
@@ -1,27 +1,20 @@
 import enchant
 from Dictionaries.dicts import dicts
-from treetaggerwrapper import TreeTagger, make_tags
-from nltk.stem.snowball import SpanishStemmer
+from lemmatizer import Lemmatizer
 
 
 class WTAclassifier(object):
 
-    def __init__(self, stem=False, tag=False):
+    def __init__(self, lemma=False):
         dictionaries = dicts()
         self.english_dict = enchant.Dict("en_EN")
         self.spanish_dict = enchant.Dict("es_AR")
         self.ND = dictionaries.norm
         self.SD = dictionaries.lemario
         self.PND = dictionaries.names
-        self.stem = stem
-        self.tag = tag
-
-        if stem:
-            self.stemmer = SpanishStemmer()
-        if tag:
-            # path to TreeTagger installation directory
-            path = 'here put path to TreeTagger'
-            self.tagger = TreeTagger(TAGLANG='es', TAGDIR=path)
+        self.lemma = lemma
+        if lemma:
+            self.lemmatizer = Lemmatizer()
 
     def dictionary_lookup(self, word):
         result = (word in self.SD or
@@ -29,26 +22,13 @@ class WTAclassifier(object):
                   word in self.ND.values())
         return result
 
-    def affix_check(self, word):
-        result = False
-        if word.islower() or word.istitle():
-            if self.stem:
-                stem = self.stemmer.stem(word)
-                n = len(stem)
-                # compare with first substring of length n of each word in SD
-                for w in [x[:n] for x in self.SD if len(x) >= n]:
-                    result = (stem == w)
-                    if result:
-                        break
-            if self.tag:
-                lemma = make_tags(self.tagger.tag_text(word))[0].lemma
-                result = self.dictionary_lookup(lemma)
-        return result
-
     def check(self, word):
         result = self.spanish_dict.check(word)
         if not result:
-            result = self.dictionary_lookup(word) or self.affix_check(word)
+            result = self.dictionary_lookup(word)
+        if not result and self.lemma:
+            lemma = self.lemmatizer.lemmatize(word)
+            result = self.dictionary_lookup(lemma)
         return result
 
     def check_NoES(self, word):
